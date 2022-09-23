ac5970191ef1a4ee12bee0453ce3729d39b85a9d
alexeikasiuk <aliakseikasiuk@gmail.com>
2022-09-23 19:27:23 +0300

    feat: add backstage block to the main page

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,3 +1,13 @@
+function showScreenSize() {
+  let width = document.documentElement.clientWidth;
+  let height = document.documentElement.clientHeight;
+  document.getElementById('screenSize').innerHTML = `${width}:${height}`;
+}
+
+window.onload = setInterval(showScreenSize, 100);
+window.resizeBy = showScreenSize;
 document.getElementById('activePage').onclick = (e) => {
   e.preventDefault();
+  console.log(window.width);
 };
+document.addEventListener('resize', showScreenSize);
