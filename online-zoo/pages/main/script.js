be4d675caa90f52999496e53bff2cfda1dc47322
alexeikasiuk <aliakseikasiuk@gmail.com>
2022-09-24 17:33:55 +0300

    feat: add 'pick and feed' section for the main page

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -6,8 +6,14 @@ function showScreenSize() {
 
 window.onload = setInterval(showScreenSize, 100);
 window.resizeBy = showScreenSize;
+
 document.getElementById('activePage').onclick = (e) => {
   e.preventDefault();
-  console.log(window.width);
+  // console.log(window.width);
 };
+
 document.addEventListener('resize', showScreenSize);
+
+document.getElementById('btnDonate').onclick = () => {
+  document.location.href = '../donate/index.html';
+};
