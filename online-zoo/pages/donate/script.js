e83a3303e9799f72ce7fc6671e7db53b13dc2c90
alexeikasiuk <aliakseikasiuk@gmail.com>
2022-09-26 21:36:20 +0300

    feat: add element's behavior and styles donate page

--- online-zoo/pages/donate/script.js
+++ online-zoo/pages/donate/script.js
@@ -4,3 +4,14 @@ for (let elem of document.querySelectorAll('.activeMenu')) {
     console.log('activeMenu click');
   };
 }
+
+document.onsubmit = (e) => {
+  e.preventDefault();
+  let amount = document.querySelector('[type="number"]').value || 0,
+    period = document.querySelector(':checked').value || 0;
+  alert(`Donate: ${amount}$ !!!\nPeriod: ${period} !`);
+};
+
+document.getElementById('btnDonate2').onclick = () => {
+  document.location.href = '../donate/index.html';
+};
