c2ebb5ba0ca3d82565fcde459a76a971926b1ad6
alexeikasiuk <aliakseikasiuk@gmail.com>
2022-09-26 16:12:15 +0300

    feat: add map page, change style & button's behavior & form validation donate page

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,25 +1,20 @@
-function showScreenSize() {
-  let width = document.documentElement.clientWidth;
-  let height = document.documentElement.clientHeight;
-  document.getElementById('screenSize').innerHTML = `${width}:${height}`;
-}
-
-window.onload = setInterval(showScreenSize, 100);
-window.resizeBy = showScreenSize;
-
 for (let elem of document.querySelectorAll('.activeMenu')) {
   elem.onclick = (e) => {
     e.preventDefault();
   };
 }
 
-document.getElementById('subscribe').onclick = (e) => {
+document.onsubmit = (e) => {
   e.preventDefault();
-  alert('Subscribe!');
+  alert(
+    `Success subscribe!!!\nemail: ${
+      document.querySelector('[type="email"]').value
+    }`
+  );
 };
-
-document.addEventListener('resize', showScreenSize);
-
+document.getElementById('goToMap').onclick = () => {
+  document.location.href = '../map/index.html';
+};
 document.getElementById('btnDonate').onclick = () => {
   document.location.href = '../donate/index.html';
 };
